4c6dc2be45918f5abeecb75da06b4a8dc0ae54b1
Christopher <qaetre@gmail.com>
2023-05-31 16:46:47 -0400

    cleaned code up

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 const targetURL = "http://localhost:3000/ramens"
 const ramenForm = document.getElementById("new-ramen")
-document.addEventListener("DOMContentLoaded", e => {
-    fetch(`${targetURL}/1`)
-    .then(response=>response.json())
-    .then(initialRamen=>updateView(e, initialRamen))
+document.addEventListener("DOMContentLoaded", () => {
     fetch(targetURL)
     .then(response=>response.json())
-    .then(ramenList=>ramenList.forEach(createRamen))
+    .then(ramenList=> {
+        updateView(ramenList[0])
+        ramenList.forEach(createRamen)
+    })
 })
 ramenForm.addEventListener("submit", e=> {
     e.preventDefault()
-    const ramenForm = document.getElementById("new-ramen")
     const submittedRamen = {}
-    submittedRamen.name = document.getElementById("new-name").value
-    submittedRamen.restaurant = document.getElementById("new-restaurant").value
-    submittedRamen.image = document.getElementById("new-image").value
-    submittedRamen.rating = document.getElementById("new-rating").value
-    submittedRamen.comment = document.getElementById("new-comment").value
+    submittedRamen['name'] = e.target['name'].value
+    submittedRamen['restaurant'] = e.target['restaurant'].value
+    submittedRamen['image'] = e.target['image'].value
+    submittedRamen['rating'] = e.target['rating'].value
+    submittedRamen['comment'] = e.target['new-comment'].value
     fetch(targetURL, { method:"POST",
     headers: {
         "Content-Type":"application/json",
@@ -26,30 +25,28 @@ ramenForm.addEventListener("submit", e=> {
     })
     .then(response=>response.json())
     .then(ramen =>createRamen(ramen))
-    ramenForm.reset()
+    e.reset()
 })
 
 
 function createRamen (ramen) {
 const ramenMenu = document.getElementById("ramen-menu")
 const newRamen = document.createElement("img")
-newRamen.src = ramen.image
+newRamen['src'] = ramen['image']
 ramenMenu.appendChild(newRamen)
-newRamen.addEventListener("click", e=>updateView(e,ramen))
+newRamen.addEventListener("click", () =>updateView(ramen))
 
 }
-function updateView (e, ramen) {
+function updateView (ramen) {
     const updateImage = document.getElementsByClassName("detail-image")
-    updateImage[0].src = ramen.image
-    updateImage[0].alt = ramen.name
+    updateImage[0]['src'] = ramen['image']
+    updateImage[0]['alt'] = ramen['name']
     const updateName = document.getElementsByClassName("name")
-    updateName[0].textContent = ramen.name
+    updateName[0].textContent = ramen['name']
     const updateRestaurant = document.getElementsByClassName("restaurant")
-    updateRestaurant[0].textContent = ramen.restaurant
+    updateRestaurant[0].textContent = ramen['restaurant']
     const updateRating = document.getElementById("rating-display")
-    updateRating.textContent = ramen.rating
+    updateRating.textContent = ramen['rating']
     const updateComment = document.getElementById("comment-display")
-    updateComment.textContent = ramen.comment
-    console.log(ramen)
-    console.log(ramen.image)
+    updateComment.textContent = ramen['comment']
 }
