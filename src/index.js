cd29b5b523bab5bc86a0422de79b73d495b1c05f
Christopher <qaetre@gmail.com>
2023-06-01 16:56:10 -0400

    add functionality to ramen menu

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const targetURL = "http://localhost:3000/ramens"
 const ramenForm = document.getElementById("new-ramen")
+const editRamenForm = document.getElementById("edit-ramen")
+const deleteRamenButton = document.getElementById("delete-ramen")
+const ramenTracker = []
+let ramenId = 0
 document.addEventListener("DOMContentLoaded", () => {
     fetch(targetURL)
     .then(response=>response.json())
@@ -25,16 +29,66 @@ ramenForm.addEventListener("submit", e=> {
     })
     .then(response=>response.json())
     .then(ramen =>createRamen(ramen))
-    e.reset()
+    ramenForm.reset()
 })
-
+editRamenForm.addEventListener("submit", e => {
+    e.preventDefault()
+    fetch(`${targetURL}/${ramenId}`, {
+        method:"PATCH",
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body: JSON.stringify({
+            rating: e.target['rating'].value,
+            comment: e.target['new-comment'].value
+        })
+    })
+    .then(response => response.json())
+    .then(ramen => {
+        updateView(ramen)
+        const ramenArray = document.getElementById("ramen-menu")
+        const updatedRamen = document.createElement("img")
+        updatedRamen['src'] = ramen['image']
+        const addId = ramen['id']
+        updatedRamen.id = `ramen${addId}`
+        updatedRamen.addEventListener("click", () =>updateView(ramen))
+        ramenTracker.forEach(function (item,index) {
+            if (item.id.toString() === updatedRamen.id) {
+                console.log(index)
+                ramenArray.replaceChild(updatedRamen, ramenArray.children[index])
+            }
+        })
+    })
+    editRamenForm.reset()
+})
+deleteRamenButton.addEventListener("click", e => deleteRamen())
+function deleteRamen() {
+fetch(`${targetURL}/${ramenId}`, {
+        method:"DELETE",
+    })
+    fetch(targetURL)
+    .then(response=>response.json())
+    .then(ramenList=> updateView(ramenList[0]))
+    ramenTracker.forEach(function (item, index) {
+        if (item.id.toString() === `ramen${ramenId}`) {
+            ramenTracker.splice(index, 1)
+            const ramenToDelete = document.getElementById(`ramen${ramenId}`)
+            ramenToDelete.remove()
+        }
+    })
+}
 
 function createRamen (ramen) {
 const ramenMenu = document.getElementById("ramen-menu")
 const newRamen = document.createElement("img")
 newRamen['src'] = ramen['image']
+const addId = ramen['id']
+newRamen.id = `ramen${addId}`
+ramenTracker.push(newRamen)
 ramenMenu.appendChild(newRamen)
 newRamen.addEventListener("click", () =>updateView(ramen))
+
 
 }
 function updateView (ramen) {
@@ -49,4 +103,5 @@ function updateView (ramen) {
     updateRating.textContent = ramen['rating']
     const updateComment = document.getElementById("comment-display")
     updateComment.textContent = ramen['comment']
+    ramenId = ramen['id']
 }
